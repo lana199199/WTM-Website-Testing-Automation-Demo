c4a81ff20a95388ccf206a7961680ff2b2903542
lana199199 <107921865+lana199199@users.noreply.github.com>
2022-10-14 07:44:00 +0700

    Add files via upload

---
+++ Libs/CustomKeywords.groovy
@@ -0,0 +1,120 @@
+
+/**
+ * This class is generated automatically by Katalon Studio and should not be modified or deleted.
+ */
+
+import java.lang.String
+
+import com.kms.katalon.core.testobject.TestObject
+
+import com.applitools.eyes.selenium.Eyes
+
+import org.openqa.selenium.WebElement
+
+import com.applitools.eyes.RectangleSize
+
+
+
+def static "com.testwithhari.katalon.plugins.Gmail.readLatestEMailBodyContent"(
+    	String emailID	
+     , 	String password	
+     , 	String folderLableName	) {
+    (new com.testwithhari.katalon.plugins.Gmail()).readLatestEMailBodyContent(
+        	emailID
+         , 	password
+         , 	folderLableName)
+}
+
+
+def static "com.testwithhari.katalon.plugins.Gmail.deleteAllEMails"(
+    	String emailID	
+     , 	String password	
+     , 	String folderLableName	) {
+    (new com.testwithhari.katalon.plugins.Gmail()).deleteAllEMails(
+        	emailID
+         , 	password
+         , 	folderLableName)
+}
+
+
+def static "com.testwithhari.katalon.plugins.Gmail.getEmailsCount"(
+    	String emailID	
+     , 	String password	
+     , 	String folderLableName	) {
+    (new com.testwithhari.katalon.plugins.Gmail()).getEmailsCount(
+        	emailID
+         , 	password
+         , 	folderLableName)
+}
+
+
+def static "com.testwithhari.katalon.plugins.Gmail.sendEmail"(
+    	String from_emailaddress	
+     , 	String email_password	
+     , 	String to_emailaddress	
+     , 	String email_subject	
+     , 	String email_body	) {
+    (new com.testwithhari.katalon.plugins.Gmail()).sendEmail(
+        	from_emailaddress
+         , 	email_password
+         , 	to_emailaddress
+         , 	email_subject
+         , 	email_body)
+}
+
+
+def static "com.kms.katalon.keyword.applitools.BasicKeywords.checkTestObject"(
+    	TestObject testObject	
+     , 	String testName	) {
+    (new com.kms.katalon.keyword.applitools.BasicKeywords()).checkTestObject(
+        	testObject
+         , 	testName)
+}
+
+
+def static "com.kms.katalon.keyword.applitools.BasicKeywords.checkWindow"(
+    	String testName	) {
+    (new com.kms.katalon.keyword.applitools.BasicKeywords()).checkWindow(
+        	testName)
+}
+
+
+def static "com.kms.katalon.keyword.applitools.BasicKeywords.checkElement"(
+    	Eyes eyes	
+     , 	WebElement element	) {
+    (new com.kms.katalon.keyword.applitools.BasicKeywords()).checkElement(
+        	eyes
+         , 	element)
+}
+
+
+def static "com.kms.katalon.keyword.applitools.EyesKeywords.eyesOpenWithBaseline"(
+    	String baselineName	
+     , 	String testName	
+     , 	RectangleSize viewportSize	) {
+    (new com.kms.katalon.keyword.applitools.EyesKeywords()).eyesOpenWithBaseline(
+        	baselineName
+         , 	testName
+         , 	viewportSize)
+}
+
+
+def static "com.kms.katalon.keyword.applitools.EyesKeywords.eyesClose"(
+    	Eyes eyes	) {
+    (new com.kms.katalon.keyword.applitools.EyesKeywords()).eyesClose(
+        	eyes)
+}
+
+
+def static "com.kms.katalon.keyword.applitools.EyesKeywords.eyesInit"() {
+    (new com.kms.katalon.keyword.applitools.EyesKeywords()).eyesInit()
+}
+
+
+def static "com.kms.katalon.keyword.applitools.EyesKeywords.eyesOpen"(
+    	String testName	
+     , 	RectangleSize viewportSize	) {
+    (new com.kms.katalon.keyword.applitools.EyesKeywords()).eyesOpen(
+        	testName
+         , 	viewportSize)
+}
