cefbab9ed4ff711dc391c441c5051a55d3b1aee2
lana199199 <107921865+lana199199@users.noreply.github.com>
2022-10-14 07:40:24 +0700

    Add files via upload

---
+++ silver-robot-katalon-test-main/Test Suites/Test1.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Test1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>64200664-2188-47aa-ae11-4403f5dced9f</testSuiteGuid>
+   <testCaseLink>
+      <guid>269af9ef-6324-4d3e-a938-275f66263cce</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/NewTestCase</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
