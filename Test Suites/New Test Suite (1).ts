a7d07a9d88707a1c7cf023db9633172f62f6fc20
thanh.pnguyen <thanh.pnguyen@10.10.34.180>
2022-10-24 15:31:07 +0700

    2

---
+++ Test Suites/New Test Suite (1).ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>New Test Suite (1)</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>c87cee06-7b21-4834-8ccf-bb221b8998f4</testSuiteGuid>
+</TestSuiteEntity>
